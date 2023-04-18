a51fc9bbd98b2e4732e122f420d3a7c22a407d82
Alif T <alift929@gmail.com>
2023-04-18 16:08:09 +0700

    menambahkan API mahasiswa

---
+++ Modul9_1302213011/Mahasiswa.cs
@@ -0,0 +1,20 @@
+﻿namespace Modul9_1302213011
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public List<string> course { get; set; }
+        public int year { get; set; }
+
+        public Mahasiswa() { }
+
+        public Mahasiswa(string nama, string nim, List<string> course, int year)
+        {
+            this.nama = nama;
+            this.nim = nim;
+            this.course = course;
+            this.year = year;
+        }
+    }
+}
